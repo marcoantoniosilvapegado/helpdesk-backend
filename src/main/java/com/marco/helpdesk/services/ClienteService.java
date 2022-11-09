b61993974c3229f5108607345260c43f2f6f14c7
marcoantoniosilvapegado <marcoantoniosilvapegado@gmail.com>
2022-11-09 18:58:28 -0300

    ajustes senha

--- src/main/java/com/marco/helpdesk/services/ClienteService.java
+++ src/main/java/com/marco/helpdesk/services/ClienteService.java
@@ -44,6 +44,10 @@ public class ClienteService {
 	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
 		objDTO.setId(id);
 		Cliente oldObj = this.findById(id);
+		
+		if(!objDTO.getSenha().equals(oldObj.getSenha())) 
+			objDTO.setSenha(encoder.encode(objDTO.getSenha()));
+		
 		validaPorCpfEEmail(objDTO);
 		oldObj = new Cliente(objDTO);
 		return repository.save(oldObj);
